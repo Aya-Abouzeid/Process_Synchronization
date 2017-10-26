a955b3ec14ad1a71df92d35d9da1b7fc4790bfec
root <aya_aly_abouzeid@yahoo.com>
2017-10-26 15:06:20 +0200

    station_load_train

--- caltrain.c
+++ caltrain.c
@@ -7,20 +7,30 @@
 void
 station_init(struct station *station)
 {
-
-station -> currentPassengers = 0;
+    station -> onBoardPassengers = 0;                       //initialize onboard passengers attribute to 0.
+    station -> waitingPassengers = 0;                       //initialize waiting passengers attribute to 0.
+    pthread_mutex_init(&(station -> trainMutex) , NULL);    //initialize train pthread mutex.
+    pthread_cond_init(&(station -> trainArrived), NULL);    //initialize train arrival pthread condition.
+    pthread_cond_init(&(station -> trainLeaving) , NULL);      //initialize train departure pthread condition.
 }
 
 void
 station_load_train(struct station *station, int count)
 {
-	if(count == 0){
-        printf("heeere\n");
-        return;
+    pthread_mutex_lock(&station -> trainMutex);             //enters critical section for a train. makes sure only one train arrive at once.
+	if(count == 0 || station -> waitingPassengers == 0 ){
+	    pthread_mutex_unlock(&station -> trainMutex);       //exits the critical section of the train if no waiting passengers or no available -
+        return;                                                //-seats on the train
     }
-    else{
-        printf("heere2\n");
+    station -> maxPassengers = count;                       //initialize maximum passengers attribute with the value sent to function.
+    pthread_cond_broadcast(&station -> trainArrived);       //broadcasts to all threads the arrival of the train.
+    while(station -> waitingPassengers != 0 && (station -> maxPassengers - station -> onBoardPassengers) > 0 ){
+        //train mutex will wait while number of waiting passengers haven't reached 0 and while free seats are available.
+        pthread_cond_wait(&station -> trainLeaving,&station -> trainMutex);
     }
+    station -> onBoardPassengers = 0;                       //resets onboard passengers attribute to 0 to be ready for another train.
+    pthread_mutex_unlock(&station -> trainMutex);           //exits the critical section of the train.
+
 }
 
 void
--- caltrain.h
+++ caltrain.h
@@ -2,8 +2,12 @@
 
 struct station {
 
-int maxPassengers;
-int currentPassengers;
+    int maxPassengers;                  //maximum number of seats in a train
+    int onBoardPassengers;              //number of passengers seated
+    int waitingPassengers;              //number of waiting passengers in the station
+    pthread_mutex_t trainMutex;
+    pthread_cond_t trainArrived;        //condition variable to indicate the arrival of a train
+    pthread_cond_t trainLeaving;           //condition variable to indicate the departure of a train
 
 };
 
