9230947a31ef07e2cb9f0bff4d26b647aa0b6e3a
root <aya_aly_abouzeid@yahoo.com>
2017-10-26 12:53:21 +0200

    CalTrain

---
+++ caltrain.c
@@ -0,0 +1,40 @@
+#include <pthread.h>
+#include "caltrain.h"
+
+
+
+
+void
+station_init(struct station *station)
+{
+
+station -> currentPassengers = 0;
+}
+
+void
+station_load_train(struct station *station, int count)
+{
+	if(count == 0){
+        printf("heeere\n");
+        return;
+    }
+    else{
+        printf("heere2\n");
+    }
+}
+
+void
+station_wait_for_train(struct station *station)
+{
+//	pthread_mutex_t mutex;
+//	pthread_cond_wait (train,mutex);
+
+
+
+}
+
+void
+station_on_board(struct station *station)
+{
+	// FILL ME IN
+}
---
+++ caltrain.h
@@ -0,0 +1,16 @@
+#include <pthread.h>
+
+struct station {
+
+int maxPassengers;
+int currentPassengers;
+
+};
+
+void station_init(struct station *station);
+
+void station_load_train(struct station *station, int count);
+
+void station_wait_for_train(struct station *station);
+
+void station_on_board(struct station *station);
